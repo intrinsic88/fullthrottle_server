e59d98167209765c1afd9f89ca4dbd8e99239b7c
Vishak Nambiar <intrinsic88@outlook.com>
2020-08-14 02:28:19 +0530

    changes for mobile

---
+++ build/precache-manifest.4ab9d3f72b391bb55a5db91d4884ccdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e63cd0d446ff0afb60113d221a5afd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc4961cb3197d0cbdf84",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de7b56a5d8b178ccfe04",
+    "url": "/static/js/2.cda7429a.chunk.js"
+  },
+  {
+    "revision": "403b11749b766c00e5b58ea377e1cb0f",
+    "url": "/static/js/2.cda7429a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc4961cb3197d0cbdf84",
+    "url": "/static/js/main.2986b13d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61ce9df1e0bc45271ba7fa73a2352799.js"
+  "/precache-manifest.4ab9d3f72b391bb55a5db91d4884ccdd.js"
 );
 
 self.addEventListener('message', (event) => {
