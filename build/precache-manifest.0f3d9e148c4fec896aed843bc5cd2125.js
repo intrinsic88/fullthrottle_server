0e59c5a7d984e07187e47691050b45c0f169f15b
Vishak Nambiar <intrinsic88@outlook.com>
2020-08-14 00:01:51 +0530

    changes for react in gitignore

---
+++ build/precache-manifest.0f3d9e148c4fec896aed843bc5cd2125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3996ee6beb6d381bc9fff3e9ab18150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc7eb60a3babacd725d7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9205b149fe6214f67bcf",
+    "url": "/static/js/2.fc909622.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/static/js/2.fc909622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7eb60a3babacd725d7",
+    "url": "/static/js/main.61942ee0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
